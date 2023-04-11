9e8e38627958745c5e7101b39fea083d5e572d93
AxeKam333 <olek.kaminski5@gmail.com>
2023-04-11 12:45:07 +0200

    bez łuków wchodzących

--- 3-graph/sortowanie-topologiczne.py
+++ 3-graph/sortowanie-topologiczne.py
@@ -2,12 +2,12 @@
 
 class Etykiety:
     def __init__(self, liczba_wierzcholkow):
-        s = []
-        f = []
-        iterator = 0
+        self.s = []
+        self.f = []
+        self.iterator = 0
         for i in range(liczba_wierzcholkow):
-            s.append(0)
-            f.append(0)
+            self.s.append(0)
+            self.f.append(0)
 
 
 class Graf_MacierzSasiedztwa:
@@ -18,12 +18,28 @@ class Graf_MacierzSasiedztwa:
     def __init__(self, macierz):
         self.macierz = macierz
 
+    def bez_łuków_wchodzacych(self):
+        a = len(self.macierz)
+        lista = []
+        for idx_sasiada in range(a):
+            jest_pusty = True
+            for idx_wierzchołka in range(a):
+                if self.macierz[idx_wierzchołka][idx_sasiada] == 1:
+                    jest_pusty = False
+                    break
+            if jest_pusty:
+                lista.append(idx_sasiada)
+        print(lista)
+
 
 class Graf_ListaNastepnikow:
 
     def __init__(self):
         self.lista = []
 
+    def __str__(self):
+        return str(self.lista)
+    
     def generate(self, maciez_sasiedztwa):
         for wierzcholek_id in range(len(maciez_sasiedztwa)):
             self.lista.append([])
@@ -31,13 +47,17 @@ class Graf_ListaNastepnikow:
                 łuk = maciez_sasiedztwa[wierzcholek_id][łuk_id]
                 if łuk != 0:
                     self.lista[wierzcholek_id].append(łuk_id)
-                    
 
-    def __str__(self) -> str:
-        return str(self.lista)
+    def topological_sort(self):
+        etykiety = Etykiety(len(self.lista))
+
+        return etykiety
+
+
 
 if __name__=="__main__":
     g = Graf_ListaNastepnikow()
-    m = Graf_MacierzSasiedztwa([[0, 1, 1, 0, 1, 1],[0, 0, 0, 1, 0, 1],[0, 0, 0, 1, 0, 0],[0, 0, 0, 0, 1, 1]])
+    m = Graf_MacierzSasiedztwa([[0, 1, 1, 0, 1, 1],[0, 0, 0, 1, 0, 1],[0, 0, 0, 1, 0, 0],[0, 0, 0, 0, 1, 1],[0,0,0,0,0,0],[0,0,0,0,1,0]])
     g.generate(m.macierz)
+    m.bez_łuków_wchodzacych()
     print(g)
