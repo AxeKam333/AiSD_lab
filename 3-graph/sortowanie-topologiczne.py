d866646be898c91b5ebc2fdd86945704d7f2b4ba
AxeKam333 <olek.kaminski5@gmail.com>
2023-04-11 17:41:58 +0200

    cyklicznosc

--- 3-graph/sortowanie-topologiczne.py
+++ 3-graph/sortowanie-topologiczne.py
@@ -22,19 +22,19 @@ class Etykiety:
     def opusc(self, v):
         self.f[v] = self.iterator
         self.iterator += 1
-
+    
+    def podstaw_pod_wzor(self, u, v):
+        s = self.s
+        f = self.f
+        return s[v] < s[u] and s[u] < f[u] and f[u] < f[v]
 
 class Graf_MacierzSasiedztwa:
-
-    def __init__(self):
-        self.macierz = []
-
-    def __init__(self, macierz):
+    def __init__(self, macierz=[]):
         self.macierz = macierz
 
     def bez_lukow_wchodzacych(self):
         a = len(self.macierz)
-        lista = []
+        dq = []
         for idx_sasiada in range(a):
             jest_pusty = True
             for idx_wierzcholka in range(a):
@@ -42,19 +42,28 @@ class Graf_MacierzSasiedztwa:
                     jest_pusty = False
                     break
             if jest_pusty:
-                lista.append(idx_sasiada)
-        return lista
+                dq.append(idx_sasiada)
+        return dq
+    
+    def czy_cykliczny(self, etykiety: Etykiety):
+        a = len(self.macierz)
+        for wierzcholek_idx in range(a):
+            for sasiad_idx in range(a):
+                if self.macierz[wierzcholek_idx][sasiad_idx] == 1 \
+                and etykiety.podstaw_pod_wzor(wierzcholek_idx, sasiad_idx):
+                    return True
+        return False
 
 
 class Graf_ListaNastepnikow:
 
-    def __init__(self):
-        self.lista = []
+    def __init__(self, lista = []):
+        self.lista = lista
 
     def __str__(self):
         return str(self.lista)
     
-    def generate(self, maciez_sasiedztwa):
+    def from_macierzsasiedztwa(self, maciez_sasiedztwa):
         for wierzcholek_id in range(len(maciez_sasiedztwa)):
             self.lista.append([])
             for luk_id in range(len(maciez_sasiedztwa[wierzcholek_id])):
@@ -94,14 +103,8 @@ class Graf_ListaNastepnikow:
                 e.opusc(wierzcholek)
                 stos.append(wierzcholek)
                 stos_pomocniczy.pop()
-        
-        
-        # e.opusc(wierzcholek)
-        # stos.append(wierzcholek)
 
-
-
-    def topological_sort(self,bez_lukow_wchodzacych):
+    def topological_sort(self,bez_lukow_wchodzacych) -> Etykiety:
         self.ety = Etykiety(len(self.lista))
         stos = []
         for i in bez_lukow_wchodzacych:
@@ -115,11 +118,41 @@ class Graf_ListaNastepnikow:
         print("kolejność topologiczna: ", stos)
         return self.ety
 
+    def czy_cykliczny(self, etykiety: Etykiety):
+        for wierzcholek_idx in range(len(self.lista)):
+            for sasiad in self.lista[wierzcholek_idx]:
+                if etykiety.podstaw_pod_wzor(wierzcholek_idx, sasiad):
+                    return True
+        return False
+            
+
+class Graf_ListaLukow:
+    
+    def __init__(self,lista = []):
+        self.lista = lista
+        
+    def from_listanastepnikow(self, listanastepnikow): 
+        self.lista = []    
+        for wierzcholek_idx in range(len(listanastepnikow)):
+            for luk in listanastepnikow[wierzcholek_idx]:
+                self.lista.append((wierzcholek_idx, luk))
+        print(self.lista)
+
+    def czy_cykliczny(self, etykiety: Etykiety, lista = []):
+        if lista != []:
+            self.__init__(lista)
+
+        for i in self.lista:
+            if etykiety.podstaw_pod_wzor(i[0],i[1]):
+                return True
+        return False
+
+
 
 if __name__=="__main__":
-    g = Graf_ListaNastepnikow()
-    # m = Graf_MacierzSasiedztwa([[0, 1, 1, 0, 1, 1],[0, 0, 0, 1, 0, 1],[0, 0, 0, 1, 0, 0],[0, 0, 0, 0, 1, 1],[0,0,0,0,0,0],[0,0,0,0,1,0]])
-    m = Graf_MacierzSasiedztwa([[0, 1, 0, 1, 0, 0, 0, 0, 0],
+    
+    m = Graf_MacierzSasiedztwa([
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
     [0, 0, 1, 1, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -127,9 +160,15 @@ if __name__=="__main__":
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 1, 0, 0, 0, 0, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]])
-    g.generate(m.macierz)
-    print(g)
-    g.topological_sort(m.bez_lukow_wchodzacych())
-    print(g.ety)
-    
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ])
+    n = Graf_ListaNastepnikow()
+    n.from_macierzsasiedztwa(m.macierz)
+    print(n)
+    etykiety = n.topological_sort(m.bez_lukow_wchodzacych())
+    print(etykiety)
+    l = Graf_ListaLukow(n.lista)
+    l.from_listanastepnikow(n.lista)
+    print(m.czy_cykliczny(etykiety))
+    print(n.czy_cykliczny(etykiety))
+    print(l.czy_cykliczny(etykiety))
