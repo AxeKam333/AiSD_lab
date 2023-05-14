9d8d57ca001c8ce9c581a7b43ddf68884f04d81a
namfohfalo <olaf@hofman.eu>
2023-05-14 12:33:33 +0200

    Update plecaki.py

--- 5-backpack/plecaki.py
+++ 5-backpack/plecaki.py
@@ -13,32 +13,49 @@ def crafting(x, tablica):
     tablica = []
     suma = 0
     for i in range(x):
-        tablica.append({'size':0, 'value':0, 'criteria':0, 'juz_uzyty':False})
+        tablica.append({'size':0, 'value':0, 'criteria':0, 'ratio':0, 'juz_uzyty':False})
         tablica[i]['size'] = random.randint(10,1000)
         tablica[i]['value'] = random.randint(100,10000)
         suma += tablica[i]['size']
     return tablica, suma
 
-def heurystyki(rzeczy, pojemnosc)
+def plecak_pojedyncze(x, rzeczy):
+    liczbaPrzedmiotow = len(rzeczy)
+    rozwiazania = [[0] * (liczbaPrzedmiotow + 1) for i in range(x + 1)]
+    for nrPrzedmiot in range(x):
+        for rozmiar in range(x + 1):
+            if (rzeczy[nrPrzedmiot]['size'] <= x):
+                if (rozwiazania[nrPrzedmiot][x - rzeczy[nrPrzedmiot]['size']] +
+                        rzeczy[nrPrzedmiot]['value'] > rozwiazania[nrPrzedmiot][x]):
+                    rozwiazania[nrPrzedmiot + 1][x] = rozwiazania[nrPrzedmiot][
+                                                                x - rzeczy[nrPrzedmiot]['value']] + \
+                                                            rzeczy[nrPrzedmiot]['value']
+                else:
+                    rozwiazania[nrPrzedmiot + 1][x] = rozwiazania[nrPrzedmiot][x]
+            else:
+                rozwiazania[nrPrzedmiot + 1][x] = rozwiazania[nrPrzedmiot][x]
+        print(rozwiazania)
+    print(rozwiazania[liczbaPrzedmiotow][pojemnoscPlecaka])
+
+def heurystyka(rzeczy, pojemnosc, rodzaj):
+    wynik = 0
+    if(rodzaj == 'size'):
+        print('size')
+    elif(rodzaj == 'value'):
+        print('value')
+    elif(rodzaj == 'ratio'):
+        print('ratio')
+        for i in range(len(rzeczy)):
+            rzeczy[i]['ratio'] = rzeczy[i]['value']/rzeczy[i]['size']
+    elif(rodzaj=='random'):
+        print('random')
 
 liczba_przedmiotow = 5
 przedmioty = []
 pojemnoscPlecaka = None
 najlepsze_rozwiazanie = 0
-kryteria=[
-    {'nazwa':'random','aktywne':True},
-    {'nazwa':'size','aktywne':True},
-    {'nazwa':'value','aktywne':True},
-    {'nazwa':'ratio','aktywne':True}]
-pojemnosci=[
-    {'wartosc':0.25,'aktywne':True},
-    {'wartosc':0.5,'aktywne':True},
-    {'wartosc':0,75,'aktywne':True}
-]
 
 for i in range(10):
     liczba_przedmiotow += i
     przedmioty, pojemnoscPlecaka = crafting(liczba_przedmiotow, przedmioty)
-    for pojemnosc in pojemnosci:
-        if(pojemnosc['aktywne']):
-            pojemnosc_robocza = pojemnoscPlecaka*pojemnosc['wartosc']
+
